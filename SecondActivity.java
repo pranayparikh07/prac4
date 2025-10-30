ed521082b809c3bbc6da5727485b9fbb1de78c36
Parikh Pranay Himanshubhai <pranayparikh07@gmail.com>
2025-10-30 19:46:55 +0530

    Create SecondActivity.java

---
+++ SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.intentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        textView = findViewById(R.id.textView);
+
+        String message = getIntent().getStringExtra("msg");
+        textView.setText("Received: " + message);
+    }
+}
